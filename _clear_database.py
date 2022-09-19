14b15d87d132c108a42e0ff9a5eedadd37d5a624
KiritoU <nguyentienduongtink21@gmail.com>
2022-09-19 21:42:09 +0700

    Done delete movies

--- _clear_database.py
+++ _clear_database.py
@@ -16,66 +16,38 @@ def delete_saved_thumbs():
 
 
 def main():
-    # delete_saved_thumbs()
+    delete_saved_thumbs()
 
     query = "SELECT p.ID FROM `ODJiM2_term_relationships` tr, ODJiM2_posts p WHERE p.ID=tr.object_id AND p.post_type='post' AND tr.term_taxonomy_id=13853"
     post_ids = database.select_with(query)
     post_ids = [x[0] for x in post_ids]
-    print(post_ids)
 
-    # for post_id in post_ids:
-    #     database.delete_from(
-    #         table=f"{CONFIG.TABLE_PREFIX}manga_volumes",
-    #         condition=f'post_id="{post_id}"',
-    #     )
-    #     database.delete_from(
-    #         table=f"{CONFIG.TABLE_PREFIX}manga_chapters",
-    #         condition=f'post_id="{post_id}"',
-    #     )
+    for post_id in post_ids:
 
-    #     _thumbnail_id = database.select_all_from(
-    #         table=f"{CONFIG.TABLE_PREFIX}postmeta",
-    #         condition=f'post_id={post_id} AND meta_key="_thumbnail_id"',
-    #     )
-    #     if _thumbnail_id:
-    #         database.delete_from(
-    #             table=f"{CONFIG.TABLE_PREFIX}posts",
-    #             condition=f"ID={_thumbnail_id[0][-1]}",
-    #         )
+        _thumbnail_id = database.select_all_from(
+            table="postmeta",
+            condition=f'post_id={post_id} AND meta_key="_thumbnail_id"',
+        )
+        if _thumbnail_id:
+            database.delete_from(
+                table="posts",
+                condition=f"ID={_thumbnail_id[0][-1]}",
+            )
 
-    #     database.delete_from(
-    #         table=f"{CONFIG.TABLE_PREFIX}postmeta",
-    #         condition=f'post_id="{post_id}"',
-    #     )
+        database.delete_from(
+            table="postmeta",
+            condition=f'post_id="{post_id}"',
+        )
 
-    #     database.delete_from(
-    #         table=f"{CONFIG.TABLE_PREFIX}term_relationships",
-    #         condition=f'object_id="{post_id}"',
-    #     )
+        database.delete_from(
+            table="term_relationships",
+            condition=f'object_id="{post_id}"',
+        )
 
-    #     database.delete_from(
-    #         table=f"{CONFIG.TABLE_PREFIX}posts",
-    #         condition=f'ID="{post_id}"',
-    #     )
-
-    # term_taxonomies = database.select_all_from(
-    #     table=f"{CONFIG.TABLE_PREFIX}term_taxonomy",
-    #     condition='taxonomy LIKE "wp-manga%"',
-    #     cols="term_taxonomy_id, term_id",
-    # )
-
-    # for term_taxonomy in term_taxonomies:
-    #     term_taxonomy_id, term_id = term_taxonomy
-
-    #     database.delete_from(
-    #         table=f"{CONFIG.TABLE_PREFIX}term_taxonomy",
-    #         condition=f"term_taxonomy_id={term_taxonomy_id}",
-    #     )
-
-    #     database.delete_from(
-    #         table=f"{CONFIG.TABLE_PREFIX}terms",
-    #         condition=f"term_id={term_id}",
-    #     )
+        database.delete_from(
+            table="posts",
+            condition=f'ID="{post_id}"',
+        )
 
 
 if __name__ == "__main__":
