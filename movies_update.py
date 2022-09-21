b1028c4d372926f23a14b4c2abff820a836efa13
KiritoU <nguyentienduongtink21@gmail.com>
2022-09-21 18:16:59 +0700

    crawl all movie and tv show

--- movies_update.py
+++ movies_update.py
@@ -1,19 +1,29 @@
+import logging
+
 from time import sleep
 
 from base import Crawler
 from settings import CONFIG
 
+logging.basicConfig(format="%(asctime)s %(levelname)s:%(message)s", level=logging.INFO)
+
 
 def main():
     crawler = Crawler()
+    i = 1
     while True:
-        url = f"{CONFIG.GO_WATCH_SERIES_HOMEPAGE}/movies?page=1"
-        soup = Crawler().crawl_soup(url)
-        if soup == 404:
-            return
+        url = f"{CONFIG.GO_WATCH_SERIES_HOMEPAGE}/movies?page={i}"
+        logging.info(f"Getting URL: {url}")
 
-        crawler.crawl_movies_on_page_with(soup)
-        sleep(CONFIG.WAIT_BETWEEN_LATEST)
+        soup = Crawler().crawl_soup(url)
+
+        if soup == 404:
+            i = 1
+        else:
+            crawler.crawl_movies_on_page_with(soup)
+            sleep(CONFIG.WAIT_BETWEEN_LATEST)
+
+            i += 1
 
 
 if __name__ == "__main__":
--- series_update.py
+++ series_update.py
@@ -1,21 +1,30 @@
+import logging
+
 from time import sleep
 
 from base import Crawler
 from settings import CONFIG
 
+logging.basicConfig(format="%(asctime)s %(levelname)s:%(message)s", level=logging.INFO)
+
 
 def main():
     crawler = Crawler()
+    i = 1
     while True:
-        url = f"{CONFIG.GO_WATCH_SERIES_HOMEPAGE}/list?type=2&page=1"
+        url = f"{CONFIG.GO_WATCH_SERIES_HOMEPAGE}/list?type=2&page={i}"
+        logging.info(f"Getting URL: {url}")
+
         soup = Crawler().crawl_soup(url)
 
         list_movies = soup.find("div", class_="list_movies")
-        if not list_movies:
-            return
 
-        crawler.crawl_series_on_page_with(list_movies)
-        sleep(CONFIG.WAIT_BETWEEN_LATEST)
+        if not list_movies:
+            i = 1
+        else:
+            crawler.crawl_series_on_page_with(list_movies)
+            sleep(CONFIG.WAIT_BETWEEN_LATEST)
+            i += 1
 
 
 if __name__ == "__main__":
